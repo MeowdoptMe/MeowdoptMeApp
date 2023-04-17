8900d5057adbe8a931e856b06ee723c28ddd4ddb
Ewa Porada <ewa@DESKTOP-UBN8MUP>
2023-04-17 20:41:29 +0200

    corrected core classes v3

--- __tests__/types.d.ts
+++ __tests__/types.d.ts
@@ -1,3 +1,27 @@
+export interface App {
+  currentScreen: Screen;
+  loggedInUser: User;
+  database: Database;
+
+  setCurrentScreen: (screen: Screen) => unknown
+}
+
+export interface Database {
+
+  getAds: () => unknown;
+  getShelters: () => unknown;
+  getUsers: () => unknown;
+  registerUser: (user: User) => unknown;
+  authUser: (user: User) => unknown;
+  getShelterData: (shelter: Shelter) => unknown;
+  getAdData: (ad: Ad) => unknown;
+  setAd: (ad: Ad) => unknown;
+  removeAd: (ad: Ad) => unknown;
+  setPermissions: (permission: Permission) => unknown;
+  registerRequest: (request: PermissionRequest) => unknown;
+  setShelter: (shelter: Shelter) => unknown;
+}
+
 export interface Ad {
   adId: number;
   pet: Pet;
@@ -41,28 +65,4 @@ export interface Photo {
 
   setImg: (img: object) => void;
   setDescription: (description: string) => void;
-}
-
-export interface App {
-  currentScreen: Screen;
-  loggedInUser: User;
-  database: Database;
-
-  setCurrentScreen: (screen: Screen) => unknown
-}
-
-export interface Database {
-
-  getAds: () => unknown;
-  getShelters: () => unknown;
-  getUsers: () => unknown;
-  registerUser: (user: User) => unknown;
-  authUser: (user: User) => unknown;
-  getShelterData: (shelter: Shelter) => unknown;
-  getAdData: (ad: Ad) => unknown;
-  setAd: (ad: Ad) => unknown;
-  removeAd: (ad: Ad) => unknown;
-  setPermissions: (permission: Permission) => unknown;
-  registerRequest: (request: PermissionRequest) => unknown;
-  setShelter: (shelter: Shelter) => unknown;
 }
