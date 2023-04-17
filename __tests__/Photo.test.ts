4fc1bd377a4bf978a61d348b6118c7ed141bb3a9
Tomasz Żelawski <tzelawski@gmail.com>
2023-04-17 18:33:35 +0200

    add describe

--- __tests__/Photo.test.ts
+++ __tests__/Photo.test.ts
@@ -1,21 +1,23 @@
-it("changes img property in Photo with setImg", () => {
-  // TODO to be corrected later on
-  let photo: Photo;
-  const img = {
-    uri: "test",
-  };
-  // @ts-expect-error
-  photo.setImg(img);
-  // @ts-expect-error
-  expect(photo.img).toBe(img);
-});
+describe("Photo class", () => {
+  it("changes img property in Photo with setImg", () => {
+    // TODO to be corrected later on
+    let photo: Photo;
+    const img = {
+      uri: "test",
+    };
+    // @ts-expect-error
+    photo.setImg(img);
+    // @ts-expect-error
+    expect(photo.img).toBe(img);
+  });
 
-it("changes description propety in Photo with setDescription", () => {
-  // TODO to be corrected later on
-  let photo: Photo;
-  const description = "test";
-  // @ts-expect-error
-  photo.setDescription(description);
-  // @ts-expect-error
-  expect(photo.description).toBe(description);
+  it("changes description propety in Photo with setDescription", () => {
+    // TODO to be corrected later on
+    let photo: Photo;
+    const description = "test";
+    // @ts-expect-error
+    photo.setDescription(description);
+    // @ts-expect-error
+    expect(photo.description).toBe(description);
+  });
 });
