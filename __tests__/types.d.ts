71ad0369bf6bce9e7f3747aed1c1d9ff85a1bad1
Ewa Porada <ewa@DESKTOP-UBN8MUP>
2023-04-20 12:05:54 +0200

    resolved conflict (try)

--- __tests__/types.d.ts
+++ __tests__/types.d.ts
@@ -55,7 +55,7 @@ export interface PhotoAlbum {
   // instead of add
   addPhoto: (photo: Photo) => void;
   // instead of delete
-  removePhoto: (photo: Photo) => void;
+  removePhoto: (index: int) => void;
 }
 
 export interface Photo {
@@ -64,4 +64,42 @@ export interface Photo {
 
   setImg: (img: object) => void;
   setDescription: (description: string) => void;
+}
+export interface Permission {
+  user: User;
+  shelter: Shelter;
+  value: number;
+}
+export interface PermissionsList {
+  permissions: Permission[];
+  removePermission: (permission: Permission) => void;
+}
+export interface PermissionRequest {
+  user: User;
+  shelter: Shelter;
+  permissionType: Permission;
+  cancelRequest: () => void;
+}
+
+export interface PreferencesScreen {
+  adFilters: AdFilters;
+  settings: AppSettings;
+
+  render: () => void;
+  setFilters: (filters: AdFilters) => void;
+  setSettings: (settings: AppSettings) => void;
+}
+
+export interface AdFilters {
+  petCharacteristics: PetCharacteristics;
+
+  setPetCharacteristics: (petCharacteristics: PetCharacteristics) => void;
+}
+
+export interface AppSettings {
+  setPassword: (password: string) => void;
+  setUsername: (username: string) => void;
+  setMail: (mail: string) => void;
+  deleteAccount: () => void;
+  logout: () => void;
 }
