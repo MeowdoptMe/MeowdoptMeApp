a4a25fcf296ac69bc6caea7b09297e01c1a3ec30
Agnieszka Trojanowska <agnieszka.t0544@gmail.com>
2023-04-27 15:28:13 +0200

    lint fix

--- __tests__/SheltersListFilters.test.ts
+++ __tests__/SheltersListFilters.test.ts
@@ -3,8 +3,8 @@ import { SheltersListFilters } from "./types";
 describe("SheltersListFilters class", () => {
   it("sets filters with setFilters", () => {
     let sheltersListFilters: SheltersListFilters;
-    const city: string = "Warszawa";
-    const maxDistance: number = 10;
+    const city = "Warszawa";
+    const maxDistance = 10;
     // @ts-expect-error
     sheltersListFilters.setFilters(city, maxDistance);
     // @ts-expect-error
