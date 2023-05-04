cd486d3efa7d3a6c8a9767dea3c0efb3278b2f5a
Gochad <malaga.drag@gmail.com>
2023-05-04 16:23:52 +0200

    :rocket:

--- BackendApp/photoAlbum/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import PhotoAlbum, Photo
-
-admin.site.register(PhotoAlbum)
-admin.site.register(Photo)
--- BackendApp/shelterRelated/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ContactInfo, Shelter
-
-admin.site.register(ContactInfo)
-admin.site.register(Shelter)
