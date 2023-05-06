26b40980fd30cf52360c9638b35293189466f2fc
Tomasz Żelawski <40713406+tjzel@users.noreply.github.com>
2023-05-06 20:42:59 +0200

    Logging in with database (#23)
    <!-- Do not remove the rocket! -->
    
    :rocket:

--- BackendApp/userAuth/serializers.py
+++ BackendApp/userAuth/serializers.py
@@ -1,35 +1,44 @@
 from rest_framework import serializers
 from .models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     email = serializers.EmailField(required=False)
+
     def validate(self, attrs):
         data = super().validate(attrs.get)
-        data['username'] = self.user.username
-        data['email'] = self.user.email
-        data['password'] = self.user.password
+        data["username"] = self.user.username
+        data["email"] = self.user.email
+        data["password"] = self.user.password
         return data
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'username', 'password']
-        extra_kwargs = {
-            'password': {'write_only': True},
-            'email': {'required': True}
-        }
+        fields = ["email", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}, "email": {"required": True}}
+
     def save(self):
-        user = User(email=self.validated_data['email'], username=self.validated_data['username'])
-        password = self.validated_data['password']
+        user = User(
+            email=self.validated_data["email"], username=self.validated_data["username"]
+        )
+        password = self.validated_data["password"]
         user.set_password(password)
         user.save()
         return user
 
+
 class PasswordChangeSerializer(serializers.Serializer):
-    current_password = serializers.CharField(style={"input_type": "password"}, required=True)
-    new_password = serializers.CharField(style={"input_type": "password"}, required=True)
+    current_password = serializers.CharField(
+        style={"input_type": "password"}, required=True
+    )
+    new_password = serializers.CharField(
+        style={"input_type": "password"}, required=True
+    )
+
     def validate_current_password(self, value):
-        if not self.context['request'].user.check_password(value):
-            raise serializers.ValidationError({'current_password': 'Does not match'})
+        if not self.context["request"].user.check_password(value):
+            raise serializers.ValidationError({"current_password": "Does not match"})
         return value
--- BackendApp/userAuth/tests.py
+++ BackendApp/userAuth/tests.py
@@ -3,46 +3,62 @@ from rest_framework.test import APITestCase, APIRequestFactory, force_authentica
 from .models import User
 from .views import RegisterView, ReturnTokenView, ChangePasswordView, LogoutView
 
+
 class UserAuthTests(APITestCase):
     def setUp(self):
         self.factory = APIRequestFactory(enforce_csrf_checks=True)
+
     def test_register_with_valid_data(self):
-        url = reverse('register')
+        url = reverse("register")
         data = {
             "email": "tdd@gmail.com",
             "username": "tdd",
             "password": "krowa12345",
         }
-        request = self.factory.post(url, data, format='json')
+        request = self.factory.post(url, data, format="json")
         view = RegisterView.as_view()
         response = view(request)
-        self.assertEqual(response.status_code, 201, f'Expected Response Code 201, received {response.status_code} instead.')
+        self.assertEqual(
+            response.status_code,
+            201,
+            f"Expected Response Code 201, received {response.status_code} instead.",
+        )
+
     def test_register_with_existing_user(self):
         User.objects.create_user(username="ewa", password="ewa12345")
-        url = reverse('register')
+        url = reverse("register")
         data = {
             "email": "tdd@gmail.com",
             "username": "ewa",
             "password": "ewa12345",
         }
-        request = self.factory.post(url, data, format='json')
+        request = self.factory.post(url, data, format="json")
         view = RegisterView.as_view()
         response = view(request)
-        self.assertEqual(response.status_code, 400, f'Expected Response Code 400, received {response.status_code} instead.')
+        self.assertEqual(
+            response.status_code,
+            400,
+            f"Expected Response Code 400, received {response.status_code} instead.",
+        )
 
     def test_register_with_additional_data(self):
-        url = reverse('register')
+        url = reverse("register")
         data = {
             "email": "tdd@gmail.com",
             "username": "ala",
             "password": "12345ala",
-            "first_name": "Ala"
+            "first_name": "Ala",
         }
-        request = self.factory.post(url, data, format='json')
+        request = self.factory.post(url, data, format="json")
         view = RegisterView.as_view()
         response = view(request)
-        self.assertEqual(response.status_code, 201, f'Expected Response Code 400, received {response.status_code} instead.')
-        self.assertEqual(User.objects.get().first_name, '')
+        self.assertEqual(
+            response.status_code,
+            201,
+            f"Expected Response Code 400, received {response.status_code} instead.",
+        )
+        self.assertEqual(User.objects.get().first_name, "")
+
     def test_login_with_valid_data(self):
         User.objects.create_user(username="ewa", password="ewa12345")
         user_data = {
@@ -50,7 +66,11 @@ class UserAuthTests(APITestCase):
             "password": "ewa12345",
         }
         response = self.login(user_data)
-        self.assertEqual(response.status_code, 200, f'Expected Response Code 200, received {response.status_code} instead.')
+        self.assertEqual(
+            response.status_code,
+            200,
+            f"Expected Response Code 200, received {response.status_code} instead.",
+        )
 
     def test_login_with_invalid_data(self):
         User.objects.create_user(username="ewa", password="ewa12345")
@@ -59,7 +79,11 @@ class UserAuthTests(APITestCase):
             "password": "ewa1234",
         }
         response = self.login(user_data)
-        self.assertEqual(response.status_code, 401, f'Expected Response Code 401, received {response.status_code} instead.')
+        self.assertEqual(
+            response.status_code,
+            401,
+            f"Expected Response Code 401, received {response.status_code} instead.",
+        )
 
     def test_login_not_existing_user(self):
         user_data = {
@@ -67,7 +91,11 @@ class UserAuthTests(APITestCase):
             "password": "ewa1234",
         }
         response = self.login(user_data)
-        self.assertEqual(response.status_code, 401, f'Expected Response Code 401, received {response.status_code} instead.')
+        self.assertEqual(
+            response.status_code,
+            401,
+            f"Expected Response Code 401, received {response.status_code} instead.",
+        )
 
     def test_change_password(self):
         user = User.objects.create_user(username="ewa", password="ewa12345")
@@ -76,17 +104,18 @@ class UserAuthTests(APITestCase):
             "password": "ewa12345",
         }
         response = self.login(user_data)
-        token = response.data['access']
-        url = reverse('change_password')
-        data = {
-            "current_password": "ewa12345",
-            "new_password": "krowa12345"
-        }
-        request = self.factory.post(url, data, format='json')
+        token = response.data["access"]
+        url = reverse("change_password")
+        data = {"current_password": "ewa12345", "new_password": "krowa12345"}
+        request = self.factory.post(url, data, format="json")
         force_authenticate(request, user=user, token=token)
         view = ChangePasswordView.as_view()
         response = view(request)
-        self.assertEqual(response.status_code, 204, f'Expected Response Code 204, received {response.status_code} instead.')
+        self.assertEqual(
+            response.status_code,
+            204,
+            f"Expected Response Code 204, received {response.status_code} instead.",
+        )
 
     def test_logout(self):
         User.objects.create_user(username="ewa", password="ewa12345")
@@ -96,17 +125,23 @@ class UserAuthTests(APITestCase):
         }
         response = self.login(user_data)
 
-        token = response.data['access']
-        url = reverse('logout')
-        request = self.factory.post(url, format='json',)
+        token = response.data["access"]
+        url = reverse("logout")
+        request = self.factory.post(
+            url,
+            format="json",
+        )
         force_authenticate(request, token=token)
         view = LogoutView.as_view()
         response = view(request)
-        self.assertEqual(response.status_code, 200,
-                         f'Expected Response Code 200, received {response.status_code} instead.')
+        self.assertEqual(
+            response.status_code,
+            200,
+            f"Expected Response Code 200, received {response.status_code} instead.",
+        )
 
     def login(self, user_data):
-        url = reverse('login')
-        request = self.factory.post(url, user_data, format='json')
+        url = reverse("login")
+        request = self.factory.post(url, user_data, format="json")
         view = ReturnTokenView.as_view()
         return view(request)
