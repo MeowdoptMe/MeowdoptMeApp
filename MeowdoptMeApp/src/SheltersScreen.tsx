ad70cde0b87353cf9adbee42eec9f2f6b838d704
trissve <86602714+trissve@users.noreply.github.com>
2023-05-08 09:54:38 +0200

    SheltersScreen with button to Map and return button (#19)
    <!-- Do not remove the rocket! -->
    
    :rocket: roket
    
    ---------
    
    Co-authored-by: Tomasz Żelawski <tzelawski@gmail.com>

--- MeowdoptMeApp/src/SheltersScreen.tsx
+++ MeowdoptMeApp/src/SheltersScreen.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {useState} from 'react';
+import {View, Text, StyleSheet, Pressable, SafeAreaView} from 'react-native';
+import {WebView} from 'react-native-webview';
+import map from './WebMap';
 
 function SheltersScreen() {
-  return (
+  const [page, setPage] = useState('list');
+  return page === 'list' ? (
     <View style={styles.sectionContainer}>
       <Text style={styles.text}>Shelters Screen</Text>
+      <View style={styles.listContainer}>
+        <Text style={styles.text}>SheltersList</Text>
+      </View>
+      <View style={styles.button}>
+        <Pressable
+          onPress={() => {
+            setPage('map');
+          }}>
+          <Text style={styles.buttonText}>Go to map</Text>
+        </Pressable>
+      </View>
     </View>
+  ) : (
+    <SafeAreaView style={styles.mapContainer}>
+      <WebView source={{html: map}} />
+      <View style={styles.button}>
+        <Pressable
+          onPress={() => {
+            setPage('list');
+          }}>
+          <Text style={styles.buttonText}>Go back</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -16,8 +43,35 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'white',
   },
+  listContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  mapContainer: {
+    flex: 1,
+  },
   text: {
     color: 'black',
+    backgroundColor: 'white',
+  },
+  button: {
+    backgroundColor: 'royalblue',
+    borderTopEndRadius: 20,
+    borderTopStartRadius: 20,
+    borderBottomEndRadius: 20,
+    borderBottomStartRadius: 20,
+    maxHeight: 40,
+    minWidth: 160,
+    flex: 1,
+    alignItems: 'center',
+  },
+  buttonText: {
+    flex: 1,
+    color: 'white',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 30,
   },
 });
 
