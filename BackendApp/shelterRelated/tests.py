a7d43fc099783b8f1f6737d0001cda06afa7722c
Gochad <malaga.drag@gmail.com>
2023-05-14 20:11:21 +0200

    add no-auth tests to shelterRelated

--- BackendApp/shelterRelated/tests.py
+++ BackendApp/shelterRelated/tests.py
@@ -30,18 +30,8 @@ class ShelterTests(APITestCase):
             "x_cord": 52.111,
             "y_cord": 26.752,
         }
-        user_data = {
-            "username": "ewa",
-            "password": "ewa12345",
-        }
-        self.user = User.objects.create_user(
-            username=user_data["username"], password=user_data["password"]
-        )
-        url = reverse_lazy("login")
-        request = self.factory.post(url, user_data, format="json")
-        view = LoginView.as_view()
-        response = view(request)
-        self.token = response.data["access"]
+        self.user = User.objects.create_user(username="ewa", password="ewa12345")
+        self.user2 = User.objects.create_user(username="gocha", password="gocha12345")
         shelter = Shelter.objects.create()
         permission_create = Permission.objects.get(codename="add_shelter")
         permission_change = Permission.objects.get(codename="change_shelter")
@@ -67,56 +57,92 @@ class ShelterTests(APITestCase):
             f"Expected Response Code 200, received {response.status_code} instead.",
         )
 
-    def test_create(self):
+    def test_create_with_auth(self):
         actual_shelter_count = Shelter.objects.count()
         self.client.force_authenticate(user=self.user)
         url = reverse_lazy("shelter_create")
         response = self.client.post(url, self.data, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, f"Expected Response Code 201, received {response.status_code} instead.",)
         self.assertEqual(Shelter.objects.count(), actual_shelter_count + 1)
         self.assertEqual(
             Shelter.objects.get(id=actual_shelter_count + 1).name, self.data["name"]
         )
-
-    def test_detail(self):
+    def test_create_with_no_auth(self):
+        actual_shelter_count = Shelter.objects.count()
+        self.client.force_authenticate(user=self.user2)
+        url = reverse_lazy("shelter_create")
+        response = self.client.post(url, self.data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN, f"Expected Response Code 403, received {response.status_code} instead.",)
+        self.assertEqual(Shelter.objects.count(), actual_shelter_count)
+    def test_detail_with_auth(self):
+        self.client.force_authenticate(user=self.user)
         Shelter.objects.create()
         url = reverse_lazy("shelter_detail", kwargs={"pk": 1})
-        request = self.factory.get(url)
-        view = ShelterDetail.as_view()
-        response = view(request, pk=1)
+        response = self.client.get(url)
         self.assertEqual(
             response.status_code,
-            200,
+            status.HTTP_200_OK,
+            f"Expected Response Code 200, received {response.status_code} instead.",
+        )
+    def test_detail_with_no_auth(self):
+        self.client.force_authenticate(user=self.user2)
+        Shelter.objects.create()
+        url = reverse_lazy("shelter_detail", kwargs={"pk": 1})
+        response = self.client.get(url)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK,
             f"Expected Response Code 200, received {response.status_code} instead.",
         )
 
-    def test_edit(self):
-        shelter = Shelter.objects.create()
+    def test_edit_with_auth(self):
+        self.client.force_authenticate(user=self.user)
+        Shelter.objects.create()
         url = reverse_lazy("shelter_detail", kwargs={"pk": 1})
         data = {}
         data["name"] = "shelter123"
-        request = self.factory.put(url, data)
-        force_authenticate(request, user=self.user, token=self.token)
-        view = ShelterDetail.as_view()
-        response = view(request, pk=1)
+        response = self.client.put(url, data)
         self.assertEqual(
             response.status_code,
-            200,
+            status.HTTP_200_OK,
             f"Expected Response Code 200, received {response.status_code} instead.",
         )
         self.assertNotEquals(Shelter.objects.get(id=1).name, self.data["name"])
-
-    def test_remove(self):
-        shelter = Shelter.objects.create()
-        current_objects_count = Shelter.objects.count()
+    def test_edit_with_no_auth(self):
+        self.client.force_authenticate(user=self.user2)
+        Shelter.objects.create()
         url = reverse_lazy("shelter_detail", kwargs={"pk": 1})
-        request = self.factory.delete(url)
-        force_authenticate(request, user=self.user, token=self.token)
-        view = ShelterDetail.as_view()
-        response = view(request, pk=1)
+        data = {}
+        data["name"] = "shelter123"
+        response = self.client.put(url, data)
         self.assertEqual(
             response.status_code,
-            204,
+            status.HTTP_403_FORBIDDEN,
+            f"Expected Response Code 403, received {response.status_code} instead.",
+        )
+        self.assertEquals(Shelter.objects.get(id=1).name, "")
+
+    def test_remove_with_auth(self):
+        self.client.force_authenticate(user=self.user)
+        Shelter.objects.create()
+        current_objects_count = Shelter.objects.count()
+        url = reverse_lazy("shelter_detail", kwargs={"pk": 1})
+        response = self.client.delete(url)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT,
             f"Expected Response Code 204, received {response.status_code} instead.",
         )
         self.assertEqual(Shelter.objects.count(), current_objects_count - 1)
+    def test_remove_with_no_auth(self):
+        self.client.force_authenticate(user=self.user2)
+        Shelter.objects.create()
+        current_objects_count = Shelter.objects.count()
+        url = reverse_lazy("shelter_detail", kwargs={"pk": 1})
+        response = self.client.delete(url)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_403_FORBIDDEN,
+            f"Expected Response Code 403, received {response.status_code} instead.",
+        )
+        self.assertEqual(Shelter.objects.count(), current_objects_count)
