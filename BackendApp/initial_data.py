beb593a746d024a5c4afcc48293157c82e5bc07b
Gochad <malaga.drag@gmail.com>
2023-05-14 13:09:59 +0200

    fix pylint errors

--- BackendApp/initial_data.py
+++ BackendApp/initial_data.py
@@ -1,14 +1,15 @@
-from userAuth.models import User
-from django.contrib.auth.hashers import make_password
 from json import load
+from django.contrib.auth.hashers import make_password
+from userAuth.models import User
 
-with open("initial_data.json") as f:
+
+with open("initial_data.json", encoding="utf-8") as f:
     users = load(f)
 
 
-def add_users(users):
-    for user_data in users:
-        user = User.objects.create(
+def add_users(user_list):
+    for user_data in user_list:
+        User.objects.create(
             username=user_data["username"],
             password=make_password(user_data["password"]),
             is_superuser=user_data["is_superuser"],
