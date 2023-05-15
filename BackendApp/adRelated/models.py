6c4b6e2113b8668eae316c7f856a19273e954e2d
Gochad <malaga.drag@gmail.com>
2023-05-15 18:55:16 +0200

    fix linting

--- BackendApp/adRelated/models.py
+++ BackendApp/adRelated/models.py
@@ -5,16 +5,16 @@ from photoAlbum.models import PhotoAlbum
 
 
 class Pet(models.Model):
-    name = models.CharField(max_length=255, default="")
-    species = models.CharField(max_length=255, default="")
-    subSpecies = models.CharField(max_length=255, default="")
-    age = models.IntegerField(default=0)
-    gender = models.CharField(max_length=255, default="")
-    color = models.CharField(max_length=255, default="")
+    name = models.CharField(max_length=255, default="", null=True)
+    species = models.CharField(max_length=255, default="", null=True)
+    subSpecies = models.CharField(max_length=255, default="", null=True)
+    age = models.IntegerField(default=0, null=True)
+    gender = models.CharField(max_length=255, default="", null=True)
+    color = models.CharField(max_length=255, default="", null=True)
 
 
 class Ad(models.Model):
     pet = models.ForeignKey(Pet, on_delete=models.CASCADE, null=False)
-    active = models.BooleanField()
+    active = models.BooleanField(null=False)
     shelter = models.ForeignKey(Shelter, on_delete=models.CASCADE, null=False)
     photo_album = models.ForeignKey(PhotoAlbum, on_delete=models.CASCADE, null=True)
