e3bf6a87a25aeccfdc6041e5913d491657aabd15
Gochad <malaga.drag@gmail.com>
2023-05-21 20:30:52 +0200

    add url missing change from master

--- BackendApp/BackendApp/urls.py
+++ BackendApp/BackendApp/urls.py
@@ -39,25 +39,11 @@ urlpatterns = [
     path("userAuth/", include("userAuth.urls")),
     path("shelters/", include("shelterRelated.urls")),
     path("permission-requests/", include("permissionHandler.urls")),
-<<<<<<< HEAD
-=======
     path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
->>>>>>> master
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
-<<<<<<< HEAD
-    re_path(
-        r"",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    re_path(
-        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
-    ),
-=======
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
->>>>>>> master
 ]
