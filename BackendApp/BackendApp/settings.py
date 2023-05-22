fef33f6476b1ef60bc60ccb15ea60f6d3efc4470
Gochad <malaga.drag@gmail.com>
2023-05-22 20:14:12 +0200

    remove upload files

--- BackendApp/BackendApp/settings.py
+++ BackendApp/BackendApp/settings.py
@@ -142,7 +142,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "userAuth.User"
 AUTH_PROFILE_MODULE = "userAuth.User"
-
-# Dir for storing data on server
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = "/media/"
