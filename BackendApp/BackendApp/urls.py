5b4c857ae4c263c5fab09d54cb44566f165ee5d2
Gochad <malaga.drag@gmail.com>
2023-05-25 18:46:36 +0200

    repair lint

--- BackendApp/BackendApp/urls.py
+++ BackendApp/BackendApp/urls.py
@@ -18,13 +18,14 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
-
-from BackendApp import settings
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+
+from BackendApp import settings
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -40,7 +41,7 @@ urlpatterns = [
         name="swagger-ui",
     ),
     path(
-        "api/schema/redoc/",
+        "redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
