585d2bc303095ea995efb793e416002ce86d1115
Gochad <malaga.drag@gmail.com>
2023-05-25 18:13:22 +0200

    add api-spectacular instead drf-yasg library for autodoc

--- BackendApp/BackendApp/urls.py
+++ BackendApp/BackendApp/urls.py
@@ -14,25 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path, include, re_path
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from django.urls import path, include
 
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
+from BackendApp import settings
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
 )
 
 
@@ -43,11 +34,6 @@ urlpatterns = [
     path("shelters/", include("shelterRelated.urls")),
     path("ads/", include("adRelated.urls")),
     path("photo-albums/", include("photoAlbum.urls")),
-    path("", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-]
+    path("", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("doc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
