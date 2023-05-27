aab06d841a77d1c173df98bd6cd6384336a6e453
trissve <e.porada8@gmail.com>
2023-05-27 10:54:37 +0200

    fix url

--- MeowdoptMeApp/src/Database.ts
+++ MeowdoptMeApp/src/Database.ts
@@ -2,7 +2,8 @@ const databaseUrl = 'http://localhost:8000/';
 const loginUrl = databaseUrl + 'user-auth/login/';
 const registerUrl = databaseUrl + 'user-auth/register/';
 const resetPasswordUrl = databaseUrl + 'user-auth/request-password-reset/';
+const getSheltersUrl = databaseUrl + 'shelters/';
 
-const Database = {loginUrl, registerUrl, resetPasswordUrl};
+const Database = {loginUrl, registerUrl, resetPasswordUrl, getSheltersUrl};
 
 export default Database;
--- MeowdoptMeApp/src/shelterUtils.tsx
+++ MeowdoptMeApp/src/shelterUtils.tsx
@@ -1,10 +1,10 @@
 import axios, { isAxiosError } from 'axios';
-import { databaseUrl } from './Database';
+import Database from './Database';
 
 
 async function getShelters() {
   try {
-    const response = await axios.get(`${databaseUrl}/shelters/`);
+    const response = await axios.get(Database.getSheltersUrl);
     //console.log(response.data)
     return response.data;
   } catch (e: unknown) {
