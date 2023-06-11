91bede508a9e0a5b45f73e52577e953b6c283c9d
Gochad <malaga.drag@gmail.com>
2023-06-11 20:57:32 +0200

    add description field to Photo

--- BackendApp/photoAlbum/models.py
+++ BackendApp/photoAlbum/models.py
@@ -7,6 +7,7 @@ class PhotoAlbum(models.Model):
 
 class Photo(models.Model):
     img = models.ImageField(upload_to="photos")
+    description = models.TextField(null=True)
     photo_album = models.ForeignKey(
         PhotoAlbum, related_name="photos", on_delete=models.CASCADE, null=True
     )
