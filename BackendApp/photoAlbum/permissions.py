6f7719798cdc1f09c8916650e1cb01bfb5e51381
Gochad <malaga.drag@gmail.com>
2023-06-14 16:20:00 +0200

    fix admin permissions to make all operations on endpoints

--- BackendApp/photoAlbum/permissions.py
+++ BackendApp/photoAlbum/permissions.py
@@ -7,57 +7,61 @@ from adRelated.models import Ad
 
 class PhotoPermission(BasePermission):
     def has_permission(self, request, view):
-        if "Create" not in view.__class__.__name__:
-            photo = view.get_object()
-            ad = Ad.objects.get(photoAlbum=photo.photo_album)
+        if not request.user.is_staff or not request.user.is_superuser:
+            if "Create" not in view.__class__.__name__:
+                photo = view.get_object()
+                ad = Ad.objects.get(photoAlbum=photo.photo_album)
 
-        permission = True
-        if request.method == "POST":
-            photo_album = PhotoAlbum.objects.get(id=view.kwargs.get("id"))
-            ad = Ad.objects.get(photoAlbum=photo_album)
-            id = Permission.objects.get(codename="add_ad")
-            permission = UserPermission.objects.filter(
-                user_id=request.user.id,
-                shelter=ad.shelter,
-                permission_id=id,
-            )
+            permission = True
+            if request.method == "POST":
+                photo_album = PhotoAlbum.objects.get(id=view.kwargs.get("id"))
+                ad = Ad.objects.get(photoAlbum=photo_album)
+                id = Permission.objects.get(codename="add_ad")
+                permission = UserPermission.objects.filter(
+                    user_id=request.user.id,
+                    shelter=ad.shelter,
+                    permission_id=id,
+                )
 
-        if request.method == "PUT":
-            id = Permission.objects.get(codename="change_ad")
-            permission = UserPermission.objects.filter(
-                user_id=request.user.id, shelter=ad.shelter, permission_id=id
-            )
+            if request.method == "PUT":
+                id = Permission.objects.get(codename="change_ad")
+                permission = UserPermission.objects.filter(
+                    user_id=request.user.id, shelter=ad.shelter, permission_id=id
+                )
 
-        if request.method == "DELETE":
-            id = Permission.objects.get(codename="delete_ad")
-            permission = UserPermission.objects.filter(
-                user_id=request.user.id, shelter=ad.shelter, permission_id=id
-            )
+            if request.method == "DELETE":
+                id = Permission.objects.get(codename="delete_ad")
+                permission = UserPermission.objects.filter(
+                    user_id=request.user.id, shelter=ad.shelter, permission_id=id
+                )
 
-        if permission:
-            return True
-        return False
+            if permission:
+                return True
+            return False
+        return True
 
 
 class PhotoAlbumPermission(BasePermission):
     def has_permission(self, request, view):
-        permission = True
+        if not request.user.is_staff or not request.user.is_superuser:
+            permission = True
 
-        if request.method == "PUT":
-            photo_album = view.get_object()
-            ad = Ad.objects.get(photoAlbum=photo_album)
-            id = Permission.objects.get(codename="change_ad")
-            permission = UserPermission.objects.filter(
-                user_id=request.user.id, shelter=ad.shelter, permission_id=id
-            )
+            if request.method == "PUT":
+                photo_album = view.get_object()
+                ad = Ad.objects.get(photoAlbum=photo_album)
+                id = Permission.objects.get(codename="change_ad")
+                permission = UserPermission.objects.filter(
+                    user_id=request.user.id, shelter=ad.shelter, permission_id=id
+                )
 
-        if request.method == "DELETE":
-            photo_album = view.get_object()
-            ad = Ad.objects.get(photoAlbum=photo_album)
-            id = Permission.objects.get(codename="delete_ad")
-            permission = UserPermission.objects.filter(
-                user_id=request.user.id, shelter=ad.shelter, permission_id=id
-            )
-        if permission:
-            return True
-        return False
+            if request.method == "DELETE":
+                photo_album = view.get_object()
+                ad = Ad.objects.get(photoAlbum=photo_album)
+                id = Permission.objects.get(codename="delete_ad")
+                permission = UserPermission.objects.filter(
+                    user_id=request.user.id, shelter=ad.shelter, permission_id=id
+                )
+            if permission:
+                return True
+            return False
+        return True
--- BackendApp/shelterRelated/permissions.py
+++ BackendApp/shelterRelated/permissions.py
@@ -5,25 +5,27 @@ from permissionHandler.models import UserPermission
 
 class ShelterPermission(BasePermission):
     def has_permission(self, request, view):
-        permission = True
-        if request.method == "POST":
-            id = Permission.objects.get(codename="add_shelter")
-            permission = UserPermission.objects.filter(
-                user_id=request.user.id, permission_id=id
-            )
+        if not request.user.is_staff or not request.user.is_superuser:
+            permission = True
+            if request.method == "POST":
+                id = Permission.objects.get(codename="add_shelter")
+                permission = UserPermission.objects.filter(
+                    user_id=request.user.id, permission_id=id
+                )
 
-        if request.method == "PUT":
-            id = Permission.objects.get(codename="change_shelter")
-            permission = UserPermission.objects.filter(
-                user_id=request.user.id, permission_id=id
-            )
+            if request.method == "PUT":
+                id = Permission.objects.get(codename="change_shelter")
+                permission = UserPermission.objects.filter(
+                    user_id=request.user.id, permission_id=id
+                )
 
-        if request.method == "DELETE":
-            id = Permission.objects.get(codename="delete_shelter")
-            permission = UserPermission.objects.filter(
-                user_id=request.user.id, permission_id=id
-            )
+            if request.method == "DELETE":
+                id = Permission.objects.get(codename="delete_shelter")
+                permission = UserPermission.objects.filter(
+                    user_id=request.user.id, permission_id=id
+                )
 
-        if permission:
-            return True
-        return False
+            if permission:
+                return True
+            return False
+        return True
