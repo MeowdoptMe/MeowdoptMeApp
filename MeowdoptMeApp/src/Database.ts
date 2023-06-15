565f6062d289b40808c223f627faa72659b41608
Tomasz Żelawski <tzelawski@gmail.com>
2023-06-15 17:51:19 +0200

    publishs

--- MeowdoptMeApp/src/Database.ts
+++ MeowdoptMeApp/src/Database.ts
@@ -1,4 +1,4 @@
-const databaseUrl = 'http://localhost:8000/';
+const databaseUrl = 'https://gochad.pythonanywhere.com/';
 const loginUrl = databaseUrl + 'user-auth/login/';
 const registerUrl = databaseUrl + 'user-auth/register/';
 const resetPasswordUrl = databaseUrl + 'user-auth/request-password-reset/';
